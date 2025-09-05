e9af1b5acac0080cc39a25e4f42a732f41a9404e
Gonzex <g.fuentes11@ufromail.cl>
2025-09-05 09:42:27 -0400

    carpeta Gonzalo1.1

---
+++ src/GF/Gonzalo.java
@@ -0,0 +1,11 @@
+package GF;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Gonzalo {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hola Mundo, soy Gonzalo Fuentes");
+    }
+}
