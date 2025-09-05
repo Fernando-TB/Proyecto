70c2a02d7762d711405322ba83faffd915e94d48
Juakamayo <j.pizarro02@ufromail.cl>
2025-09-05 09:53:43 -0400

    init: hola mundo :3

--- src/JP/Java3.java
+++
@@ -1,4 +0,0 @@
-package JP;
-
-public class Java3 {
-}
---
+++ src/JP/Joaquin.java
@@ -0,0 +1,7 @@
+package JP;
+public class Joaquin {
+    public static void main(String[] args) {
+
+        System.out.printf("Hola Mundo, soy Joaquin Pizarro");
+    }
+}
